aab9f366051e552b15027e686fb49e3ea12ff6ac
Gabriel Tiossi <gabrieltiossi@gmail.com>
2018-11-18 16:10:44 -0200

    Adding endpoint count metrics

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
 from werkzeug.wsgi import DispatcherMiddleware
-from prometheus_client import make_wsgi_app
+from prometheus_client import Counter, Summary, make_wsgi_app
 from flask import Flask, jsonify
 from envparse import env
 import os
 
 
+APP_NAME = 'ping'
 app = Flask(__name__)
+
+required_prometheus_labels = ['endpoint', 'app', 'hostname']
+AMOUNT_TIMES_CALLED = Counter('amount_times_called', 'Amount of times a function was called', required_prometheus_labels)
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request', required_prometheus_labels)
 
 # Necessary to expose Prometheus metrics
 app_dispatch = DispatcherMiddleware(
     app,
     {
-        '/metrics': make_wsgi_app()
+        '/metrics': make_wsgi_app(),
     },
 )
+
+
+def get_labels_dict(*metric_values):
+    labels = {
+        "app": APP_NAME,
+        "hostname": os.environ['HOSTNAME'],
+    }
+    for i, value in enumerate(metric_values):
+        labels[required_prometheus_labels[i]] = value
+    return labels
+
 
 @app.route('/')
 def root():
@@ -22,25 +38,26 @@ def root():
 
 @app.route('/hello/<user>')
 def say_hello_user(user):
-    hello_message = f"Hello {user} NEW VERSION!"
+    labels = get_labels_dict(f"/hello/{user}")
+    AMOUNT_TIMES_CALLED.labels(**labels).inc()
+
+    hello_message = f"Hello {user}"
     return hello_message, 200
 
 
 @app.route('/health')
 def get_health():
+    labels = get_labels_dict("/health")
+    AMOUNT_TIMES_CALLED.labels(**labels).inc()
+
     health_message = "You should try Ping Pong!"
     return health_message, 200
 
 
 @app.route('/metrics')
 def metrics():
-    APP_COMMIT = env.str(
-        'APP_COMMIT', default="no-commit-specified"
-    )
-    desired_metrics = {
-        "APP_COMMIT": APP_COMMIT,
-    }
-    return jsonify(desired_metrics)
+    labels = get_labels_dict("/metrics")
+    AMOUNT_TIMES_CALLED.labels(**labels).inc()
 
 
 if __name__ == '__main__':
